8ebb25eb97d44820aabd5ca0ea079bb7194678fa
paul kairu <paulkairukamau@gmail.com>
2024-11-09 12:10:57 +0300

    react-hooks-event-handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  // Event handler for the 'focus' event
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Event handler for the 'blur' event
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button 
+      onFocus={handleFocus} 
+      onBlur={handleBlur}
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
